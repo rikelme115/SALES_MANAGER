8095b9e2d232831c64ea7472d555a3ffe47e0ca3
rikelme115 <rikelme115@gmail.com>
2021-09-22 14:26:58 -0500

    RALM delete imports innecesary

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import './App.css';
 
-//import Acordeon from './componentes/Acordeon';
-//import Alertas  from './componentes/Alertas';
-//import Badge    from './componentes/Badge';
-//import Login from './componentes/Login/Login';
-//import LoginIcon from './componentes/Login/Icon';
-//import ImgLogin from './componentes/Login/Img';
+
 import LoginB from './componentes/Login/LoginB';
 
 
